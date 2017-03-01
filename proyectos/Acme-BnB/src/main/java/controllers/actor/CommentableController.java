1cd9bf899e747bc583f0aeec9f8fd3d3cb63c997
AleGamero <Mastercadena@hotmail.com>
2017-03-01 18:12:27 +0100

    Bumbum

--- proyectos/Acme-BnB/src/main/java/controllers/actor/CommentableController.java
+++ proyectos/Acme-BnB/src/main/java/controllers/actor/CommentableController.java
@@ -6,10 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import services.CommentableService;
+import controllers.AbstractController;
 
 @Controller
 @RequestMapping("/commentable/actor")
-public class CommentableController {
+public class CommentableController extends AbstractController {
 
 	// Services -----------------------------------------------------------------------
 	@Autowired
