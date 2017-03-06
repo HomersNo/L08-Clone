809520a14a1cfab3a11ed9f6357de725bb426d6d
Silverath <pabvazzam@gmail.com>
2017-03-06 18:04:59 +0100

    blae

--- proyectos/Acme-BnB/src/main/java/repositories/InvoiceRepository.java
+++ proyectos/Acme-BnB/src/main/java/repositories/InvoiceRepository.java
@@ -1,7 +1,5 @@
 package repositories;
 
-import java.util.Collection;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,8 +9,8 @@ import domain.Invoice;
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
 	
-	@Query("select count(r.invoice)*1.0 from Request r group by r.tenant")
-	Collection<Double> findAvgMinMaxPerTenant();
+	@Query("select count(r.invoice) from Request r group by r.tenant")
+	Double[] findAvgMinMaxPerTenant();
 	
 	@Query("select sum(i.totalAmount) from Invoice i")
 	Double findTotalMoneyDue();
